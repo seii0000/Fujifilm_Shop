29d437fb606fbec749619edf66bc836434c1be8d
seii0000 <thanhkthp2710@gmail.com>
2024-12-04 23:10:37 +0700

    refactor: improve header/footer loading and add tab functionality

--- js/loadHeaderFooter.js
+++ js/loadHeaderFooter.js
@@ -1,13 +1,29 @@
 document.addEventListener("DOMContentLoaded", function() {
-    fetch('/Fujifilm_Shop/page/header.html')
-      .then(response => response.text())
-      .then(data => {
-        document.getElementById('header-placeholder').innerHTML = data;
-      });
-  
-    fetch('/Fujifilm_Shop/page/footer.html')
-      .then(response => response.text())
-      .then(data => {
-        document.getElementById('footer-placeholder').innerHTML = data;
-      });
-  });
+  fetch('/Fujifilm_Shop/page/header.html')
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.text();
+    })
+    .then(data => {
+      document.getElementById('header-placeholder').innerHTML = data;
+    })
+    .catch(error => {
+      console.error('There was a problem with the fetch operation:', error);
+    });
+
+  fetch('/Fujifilm_Shop/page/footer.html')
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.text();
+    })
+    .then(data => {
+      document.getElementById('footer-placeholder').innerHTML = data;
+    })
+    .catch(error => {
+      console.error('There was a problem with the fetch operation:', error);
+    });
+});
--- js/main.js
+++ js/main.js
@@ -1,4 +1,38 @@
 document.addEventListener("DOMContentLoaded", function() {
+    // Lấy tất cả các tab liên quan đến form
+    const tabLinks = document.querySelectorAll('.nav-link');
+    const tabPanes = document.querySelectorAll('.tab-pane');
+    
+    // Xử lý sự kiện khi nhấn vào các tab
+    // Đặt tab đầu tiên luôn được active khi mở login form
+    tabLinks[0].classList.add('active');
+    tabPanes[0].classList.add('active', 'show');
+
+    tabLinks.forEach(tab => {
+      tab.addEventListener('click', function(event) {
+      event.preventDefault(); // Ngăn chặn hành vi mặc định của thẻ a khi click vào nó
+      
+      // Loại bỏ class 'active' và 'show' khỏi tất cả các tab
+      tabLinks.forEach(link => {
+        link.classList.remove('active');
+      });
+      tabPanes.forEach(pane => {
+        pane.classList.remove('active', 'show');
+      });
+      
+      // Thêm class 'active' vào tab vừa được click
+      tab.classList.add('active');
+      
+      // Lấy ID của tab đã chọn
+      const targetTabId = tab.getAttribute('href').substring(1); // Lấy id của tab (bỏ dấu '#')
+      
+      // Thêm class 'active' và 'show' vào tab-pane tương ứng
+      const targetTabPane = document.getElementById(targetTabId);
+      targetTabPane.classList.add('active', 'show');
+      });
+    });
+  
+
   // Get the button
   let mybutton = document.getElementById("btn-back-to-top");
 
